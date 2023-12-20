169cf1b36e5130206ec221abff6a794f4e0f081e
NPAishwarya18 <122689983+NPAishwarya18@users.noreply.github.com>
2023-12-20 10:08:10 +0530

    Create p2.py

---
+++ p2.py
@@ -0,0 +1,19 @@
+import numpy as np
+a=np.array([1,2,3,4,5])
+b=np.array([6,7,8,9,10])
+
+print("Array a:",a)
+print("Array b:",b)
+print("sum of array a and b:",np.add(a,b))
+print("difference of array a and b:",np.subtract(a,b))
+print("product of array a and b:",np.multiply(a,b))
+print("Division of array a and b:",np.divide(a,b))
+print("square root of array a:",np.sqrt(a))
+print("exponential of array a :",np.exp(a))
+
+print("Minimum value of array a:",np.min(a))
+print("Maximum value of array b:",np.max(b))
+print("Mean value of array a:",np.mean(a))
+print("SD value of array b:",np.std(b))
+
+
